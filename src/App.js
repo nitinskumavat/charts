55cfffd375950677f90dea70b9e227c8333b0f44
Nitin Kumavat <nitin.s.kumavat@gmail.com>
2020-11-30 03:02:36 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import {LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid} from 'recharts'
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[error,setError]=useState(null);
+  const[isLoaded,setIsLoaded]=useState(false);
+  const [data,setData]=useState([]);
+
+  const getData=()=>{
+    fetch("https://qcroy2b7qg.execute-api.ap-south-1.amazonaws.com/prod/data")
+      .then(res=>res.json())
+      .then((result)=>{
+        setIsLoaded(true)
+        setData(result)
+      },
+      (error)=>{
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }
+
+  useEffect(()=>{
+    getData()
+    const interval=setInterval(()=>{
+      getData();
+    },3*60*1000);
+    return ()=>clearInterval(interval)
+  },[])
+  
+  if(error){
+    return <div className="error">Error: {error.message}</div>
+  }
+  else if(!isLoaded){
+    return (<div className="loading">Loading.......</div>)
+  }
+  else return(
+    <div>
+      <div className="heading"><h1>Graph</h1></div>
+      <LineChart width={1300} height={500} data={data}
+          margin={{top: 15, right: 20, left: 20, bottom: 5}} >
+        <XAxis dataKey="month"/>
+        <YAxis/>
+        <CartesianGrid strokeDasharray="3 3"/>
+        <Tooltip/>
+        <Line type="monotone" dataKey="cupcake" stroke="#8884d8"  activeDot={{r: 7}}/>
+      </LineChart>
+      <div className="info">Graph will be updated at interval of <span>&#8773;</span>5 minutes</div>
     </div>
-  );
+
+  )
 }
 
 export default App;
